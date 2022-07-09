53f37eee0f5fae337ac45e31239c4014e4617a85
Chenwi Eugene <chenwieugene.j@gmail.com>
2022-07-09 14:34:57 +0100

    added firebase configs and installed firebase

---
+++ firebaseconfigs.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+
+import { initializeApp } from "firebase/app";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCgtGec6LNE8ZpQYOmmJuRPejVuzwS5Qwo",
+  authDomain: "lost-and-found-8ef8a.firebaseapp.com",
+  projectId: "lost-and-found-8ef8a",
+  storageBucket: "lost-and-found-8ef8a.appspot.com",
+  messagingSenderId: "80767057440",
+  appId: "1:80767057440:web:fdd0338525a09275e48645",
+  measurementId: "G-D05Z8X12XM"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
