866b98971ac1817ae56bfcb5116d926538fdb2bd
chenwi32 <chenwieugene.j@gmail.com>
2022-10-28 10:18:37 +0100

    fixed image issues

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+
 import Link from "next/link";
 import styles from "./styles/Navigation.module.css";
 
@@ -7,10 +7,6 @@ const Navigation = () => {
     <div className={`${styles.navbar} flex`}>
       <Link href="/">
      <a> 
-          {/* <Image src="/images/logo.png"
-            width={100}
-            height={50}
-          /> */}
           237L&F
       </a>
       </Link>
