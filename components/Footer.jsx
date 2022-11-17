5c2628aa583a662f94114ed8c6efefa2bc3ad84d
chenwi32 <chenwieugene.j@gmail.com>
2022-11-17 07:59:50 +0100

    fixed some layout issues

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,18 +1,20 @@
+import { Container } from '@chakra-ui/react';
 import styles from './styles/Footer.module.css'
 
 const Footer = () => {
     return (
-      <div className={`${styles.footer} flex`}>
-        <div className="container">
+      <Container p={'1rem 0'} maxW={'100%'} className={`${styles.footer} flex`}>
+        
+        <Container maxW={1200} m={'auto'}>
           <div className="left">
           <div className="social">
             <i className='fa fa-facebok'></i>
           </div>
-          <p>Copy<sup>&#x00A9;</sup> right 2022 | All Rights Reserved.</p> 
+          <p>Copy<sup>&#x00A9;</sup> right 2022 | 237L&F All Rights Reserved.</p> 
         </div>
         <div className="right"></div>
-      </div>
-        </div>
+      </Container>
+        </Container>
         
     );
 }
--- components/avilableDocs.jsx
+++ components/avilableDocs.jsx
@@ -2,7 +2,7 @@ import { Container, Heading } from "@chakra-ui/react";
 
 const AvilableDocs = () => {
     return (
-        <Container maxW={1200} mt={10} >
+        <Container p={0} maxW={1200} mt={10} >
             <Heading>
                 Found Documents
             </Heading>
