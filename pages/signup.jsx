be00e5eb548b4826cfd351a6b678ebd10a4bf718
chenwi32 <chenwieugene.j@gmail.com>
2022-12-22 06:50:35 +0100

    created a functionality to view password

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -1,11 +1,25 @@
-import { Container, Heading, Text, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  HStack,
+  Icon,
+  Text,
+  useMediaQuery,
+  VStack,
+} from "@chakra-ui/react";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
 import { auth } from "../firebase";
 
 const Signup = () => {
+  const [isLargerThan700] = useMediaQuery("(min-width: 700px)");
   const [email, setEmail] = useState();
   const [password, setPassWord] = useState();
+  const [isVisible, setVisible] = useState(false);
 
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
@@ -25,18 +39,17 @@ const Signup = () => {
       minH={"70vh"}
       w="100%"
       bg={"brand.400"}
-      color="brand.600"
       mb={10}
       borderBottomLeftRadius={"10rem"}
-      borderTopRightRadius={"10rem"}
+      borderTopRightRadius={isLargerThan700 ? "10rem" : 0}
     >
       <Container m="auto" maxW={700} p={5} alignItems>
         <Heading color={"brand.200"} fontSize={"1.5rem"} mb={5}>
           Sign Up
         </Heading>
-        <VStack w={'100%'} alignItems={"flex-start"}  >
+        <VStack w={"100%"} alignItems={"flex-start"}>
           <VStack alignItems={"flex-start"}>
-            <Text>Email:</Text>
+            <Text color="brand.600">Email:</Text>
             <input
               onChange={(e) => {
                 setEmail(e.target.value);
@@ -50,18 +63,42 @@ const Signup = () => {
           </VStack>
 
           <VStack alignItems={"flex-start"}>
-            <Text>Password:</Text>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => {
-                setPassWord(e.target.value);
-              }}
-              placeholder="Password"
-              className={`main_input `}
-              
-              required
-            />
+            <Text color="brand.600">Password:</Text>
+
+            <Flex gap={0} p={0} h="fit-content">
+              <input
+                type={isVisible ? "text" : "password"}
+                value={password}
+                onChange={(e) => {
+                  setPassWord(e.target.value);
+                }}
+                placeholder="Password"
+                className={`main_input `}
+                required
+              />
+
+              <Flex
+                color="brand.400"
+                p={"0.2rem 0.5rem"}
+                alignItems={"center"}
+                ml={"-2rem"}
+                borderTopRightRadius={5}
+                borderBottomRightRadius={5}
+              >
+                <Icon
+                  fontSize={"1rem"}
+                  onClick={() => {
+                    if (isVisible) {
+                      setVisible(false);
+                    } else {
+                      setVisible(true)
+                    }
+                  }}
+                >
+                  <FontAwesomeIcon icon={isVisible? faEyeSlash : faEye} />
+                </Icon>
+              </Flex>
+            </Flex>
           </VStack>
         </VStack>
       </Container>
