25a9c44f5cd080f62d6c7f6a1280ecb52746f8cc
nforne <mnforne@gmail.com>
2023-02-04 16:38:09 -0500

    split css and js from html for scalability and debugging

--- firebaseconfigs.js
+++ firebaseconfigs.js
@@ -1,5 +1,5 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyCgtGec6LNE8ZpQYOmmJuRPejVuzwS5Qwo",
   authDomain: "lost-and-found-8ef8a.firebaseapp.com",
   projectId: "lost-and-found-8ef8a",
@@ -8,7 +8,4 @@ const firebaseConfig = {
   appId: "1:80767057440:web:fdd0338525a09275e48645",
   measurementId: "G-D05Z8X12XM"
 };
-
-
-export default firebaseConfig
 
