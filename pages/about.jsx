06fce19c06426c8107195375a8acb8fc926cfc3b
chenwi32 <chenwieugene.j@gmail.com>
2023-05-03 05:46:35 +0100

    Adjusted mobile view of the about page and started the how to claim documentation

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,10 +1,24 @@
-import { Box, Container, Flex, Heading, Image, ListItem, Text, UnorderedList } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  Image,
+  Link,
+  ListItem,
+  Text,
+  UnorderedList,
+  useMediaQuery,
+} from "@chakra-ui/react";
 
 import Description from "../components/description";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { faArrowDown, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 const About = () => {
+
+  const [isLargerThan700] = useMediaQuery("(min-width: 700px)");
+
   return (
     <Container mt={20} mb={10} maxW={900}>
       <Heading
@@ -41,6 +55,9 @@ const About = () => {
           </ListItem>
           <ListItem>
             <a href="#register">Register a found document</a>{" "}
+          </ListItem>
+          <ListItem>
+            <a href="#claim">Claim a found document</a>
           </ListItem>
         </UnorderedList>
       </Box>
@@ -87,8 +104,11 @@ const About = () => {
           alt=""
         />
 
-        <Text>After clicking on the Google button, a window will open and you can select your google account. The page will look like this;</Text> 
- <Image
+        <Text>
+          After clicking on the Google button, a window will open and you can
+          select your google account. The page will look like this;
+        </Text>
+        <Image
           src="/images/pic-14.png"
           h={300}
           m={"auto"}
@@ -98,7 +118,9 @@ const About = () => {
         />
         <Text>
           After logging in, You will then be redirected to the page where you
-          can be able to register the document. You can then fill out the infomation and required, and then choose a picture of the document from your device. This is what it will look like;
+          can be able to register the document. You can then fill out the
+          infomation and required, and then choose a picture of the document
+          from your device. This is what it will look like;
         </Text>
         <Image
           src="/images/pic-13.png"
@@ -109,31 +131,56 @@ const About = () => {
           alt=""
         />
 
-        <Text>After choosing the image, you should then click the upload button and then click on send respectively as indicated below.</Text>
+        <Text>
+          After choosing the image, you should then click the upload button and
+          then click on send respectively as indicated below.
+        </Text>
 
-        <Flex alignItems={'center'} gap={2}>
+        <Flex flexDirection={isLargerThan700 ? 'row' : "column"} alignItems={"center"} gap={2}>
           <Image
-          src="/images/pic-12.png"
-          h={300}
-          m={"auto"}
-          mt={10}
-          mb={10}
-          alt=""
+            src="/images/pic-12.png"
+            h={300}
+            m={"auto"}
+            mt={10}
+            mb={10}
+            alt=""
           />
-          <Text fontSize={'2rem'}>
-             <FontAwesomeIcon icon={faArrowRight}/>
+          <Text fontSize={"2rem"}>
+            <FontAwesomeIcon icon={ isLargerThan700 ? faArrowRight : faArrowDown} />
           </Text>
-         
+
           <Image
-          src="/images/pic-11.png"
-          h={300}
-          m={"auto"}
-          mt={10}
-          mb={10}
-          alt=""
-        />
+            src="/images/pic-11.png"
+            h={300}
+            m={"auto"}
+            mt={10}
+            mb={10}
+            alt=""
+          />
         </Flex>
-        <Text>After clicking send, a confirmation window will pop, just check if everything is okay and send.</Text>
+        <Text>
+          After clicking send, a confirmation window will pop, just check if
+          everything is okay and send and that is it. if everything goes well,
+          then you have successfully registered a found document and you will
+          see a success alert, otherwise, please{" "}
+          <Link href="/contact" color={"brand.100"}>
+            get in touch
+          </Link>{" "}
+          and let us know.
+        </Text>
+      </Box>
+
+      <Box>
+        <Heading
+          color={"brand.400"}
+          fontSize={"1.5rem"}
+          mt={10}
+          mb={5}
+          fontFamily="Andika"
+          id="claim"
+        >
+          How to claim a found document.
+        </Heading>
       </Box>
     </Container>
   );
