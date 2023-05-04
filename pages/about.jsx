871a7b3c03dacf07dd429b4eec0f7006fbb85501
chenwi32 <chenwieugene.j@gmail.com>
2023-05-04 14:05:39 +0100

    worked on the documentation

--- pages/about.jsx
+++ pages/about.jsx
@@ -193,7 +193,172 @@ const About = () => {
 
         <Text>
           To claim a document, you can go to the feeds section and srcoll
-          through the available documents or you to the lost
+          through the available documents or you go to the lost ID card page and
+          type your ID name or number to search with the search engine, more on
+          this below.
+        </Text>
+
+        <Heading
+          fontSize={"1.2rem"}
+          fontFamily={"Andika"}
+          color={"brand.500"}
+          mt={10}
+          mb={5}
+        >
+          Using the feeds page to look for your ducument.
+        </Heading>
+
+        <Text>
+          To access the page, you just have to navigate to it by clicking the
+          menu icon on mobile device, or just click feeds in the menu on top if
+          you are on your computer.
+        </Text>
+        <Flex
+          flexDirection={isLargerThan700 ? "row" : "column"}
+          alignItems={"center"}
+          gap={2}
+        >
+          <Image
+            src="/images/pic-1.png"
+            h={300}
+            m={"auto"}
+            mt={10}
+            mb={10}
+            alt=""
+          />
+          <Text fontSize={"2rem"}>
+            <FontAwesomeIcon
+              icon={isLargerThan700 ? faArrowRight : faArrowDown}
+            />
+          </Text>
+
+          <Image
+            src="/images/pic-10.png"
+            h={300}
+            m={"auto"}
+            mt={10}
+            mb={10}
+            alt=""
+          />
+        </Flex>
+
+        <Text>
+          When you find your Document, you click on the "This is my ID card
+          button", when you do, a dialogue box will pop up asking for your phone
+          number, enter a number that you be easily reached through, and click
+          "Proceed"
+        </Text>
+
+        <Flex
+          flexDirection={isLargerThan700 ? "row" : "column"}
+          alignItems={"center"}
+          gap={2}
+        >
+          <Image
+            src="/images/pic-8.png"
+            h={300}
+            m={"auto"}
+            mt={10}
+            mb={10}
+            alt=""
+          />
+          <Text fontSize={"2rem"}>
+            <FontAwesomeIcon
+              icon={isLargerThan700 ? faArrowRight : faArrowDown}
+            />
+          </Text>
+
+          <Image
+            src="/images/pic-6.png"
+            h={300}
+            m={"auto"}
+            mt={10}
+            mb={10}
+            alt=""
+          />
+        </Flex>
+
+        <Text>
+          If everything goes well, you will recieve a success notification that
+          looks like this below, otherwise, please{" "}
+          <Link href="/contact" color={"brand.100"}>
+            get in touch
+          </Link>{" "}
+          and let us know.
+        </Text>
+
+        <Image
+          src="/images/pic-5.png"
+          h={300}
+          m={"auto"}
+          mt={10}
+          mb={10}
+          alt=""
+        />
+
+        <Text>
+          After that, all you have to do is to wait for maximum 48 hours, if you
+          are not contacted, please still get back to us.
+        </Text>
+
+        <Heading
+          fontSize={"1.2rem"}
+          fontFamily={"Andika"}
+          color={"brand.500"}
+          mt={10}
+          mb={5}
+        >
+          Using the search engine.
+        </Heading>
+
+        <Text>
+          To use the search engine, you can click on the "lost ID card " button
+          as indicated below.
+        </Text>
+
+        <Image
+          src="/images/pic-16.png"
+          h={300}
+          m={"auto"}
+          mt={10}
+          mb={10}
+          alt=""
+        />
+
+        <Text>
+          From there , You can switch between the pannels to use either your
+          name or ID card number.
+        </Text>
+
+        <Flex
+          flexDirection={isLargerThan700 ? "row" : "column"}
+          alignItems={"center"}
+          gap={2}
+        >
+          <Image
+            src="/images/pic-4.png"
+            h={300}
+            m={"auto"}
+            mt={10}
+            mb={10}
+            alt=""
+          />
+          <Text fontSize={"2rem"}>
+            OR
+          </Text>
+
+          <Image
+            src="/images/pic-2.png"
+            h={300}
+            m={"auto"}
+            mt={10}
+            mb={10}
+            alt=""
+          />
+        </Flex>
+
+        <Text>
+          After typing the number or the name, you click on search, and just wait for the results to be displayed below the search bar. You can then claim your document from there the same way I explained above in the feeds page method.
         </Text>
       </Box>
     </Container>
